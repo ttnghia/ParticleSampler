aff9ff9f7146f9dbc17fe8232ee261f8805382cd
Nghia Truong <nghiatruong.vn@gmail.com>
2018-06-14 14:33:14 -0600

    :bouquet: add control for number of running threads

--- Source/Main.cpp
+++ Source/Main.cpp
@@ -19,6 +19,9 @@
 //-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
 //-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
 
+#define TBB_PREVIEW_GLOBAL_CONTROL 1
+#include "tbb/global_control.h"
+
 #include "MainWindow.h"
 
 #include <Banana/Utils/AppConfigReader.h>
@@ -27,5 +30,14 @@
 //-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
 int main(int argc, char* argv[])
 {
+    size_t          parallelism;
+    AppConfigReader config("config.ini");
+    if(config.isFileLoaded() && config.hasParam("NumThreads")) {
+        parallelism = static_cast<size_t>(config.getIntValue("NumThreads"));
+    } else {
+        parallelism = tbb::task_scheduler_init::default_num_threads();
+    }
+    tbb::global_control c(tbb::global_control::max_allowed_parallelism, parallelism);
+    (void)c;
     __BNNQt_RunMainWindow(MainWindow, argc, argv, true);
 }
